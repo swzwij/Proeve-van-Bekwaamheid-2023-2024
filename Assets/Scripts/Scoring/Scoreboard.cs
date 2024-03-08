0d04c88dbfe83e93248a7669962e25351def965c
Samuel <70766878+swzwij@users.noreply.github.com>
2024-03-08 11:50:13 +0100

    added meta saving

--- Assets/Scripts/Scoring/Scoreboard.cs
+++ Assets/Scripts/Scoring/Scoreboard.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace UntitledCube.Scoring
@@ -53,6 +55,78 @@ namespace UntitledCube.Scoring
 
             dictionary.Remove(key);
             Save(dictionary);
+        }
+
+        public static MetaData MetaData;
+
+        public static string AssignmentText { get; set; }
+        public static string Name { get; set; }
+        public static string Path { get; set; }
+        public static string SaveDate { get; set; }
+        public static Texture2D Texture2D { get; set; }
+        public static Picture Thumbnail { get; set; }
+
+        public static MetaData LoadMetaData(string filePath)
+        {
+            string metaDataName = System.IO.Path.GetFileName(filePath).Replace(".jpg", ".dat");
+
+            string folderPath = Path.Replace(System.IO.Path.GetFileName(filePath), "");
+            string metaDataPath = System.IO.Path.Combine(folderPath, "MetaData", metaDataName);
+
+            FileStream file;
+
+            if (File.Exists(metaDataPath)) file = File.OpenRead(metaDataPath);
+            else
+            {
+                Debug.LogError($"{metaDataPath} not found");
+
+                return MetaData;
+            }
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            MetaData data = (MetaData)formatter.Deserialize(file);
+            file.Close();
+
+            return data;
+        }
+
+        public static void SaveMetaData()
+        {
+            string metaDataName = Name.Replace(".jpg", ".dat");
+
+            string folderPath = Path.Replace(System.IO.Path.GetFileName(Path), "");
+            string metaDataPath = System.IO.Path.Combine(folderPath, "MetaData");
+
+            if (!Directory.Exists(metaDataPath))
+            {
+                Directory.CreateDirectory(metaDataPath);
+            }
+
+            metaDataPath = System.IO.Path.Combine(metaDataPath, metaDataName);
+
+            FileStream file = File.Exists(metaDataPath) ? File.OpenWrite(metaDataPath)
+                                                        : File.Create(metaDataPath);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(file, MetaData); // 'MetaData' struct now includes our dictionary
+            file.Close();
+        }
+
+        private static MetaData SetMetaData()
+        {
+            MetaData.AssignmentText = AssignmentText;
+            MetaData.Name = Name;
+            MetaData.Path = Path;
+            MetaData.ThumbnailName = Thumbnail.Name;
+            MetaData.ThumbnailPath = Thumbnail.Path;
+            MetaData.MetaDataName = Name.Replace(".jpg", ".dat");
+            MetaData.MetaDataPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "MetaData", MetaData.MetaDataName);
+            MetaData.SaveDate = SaveDate;
+
+            // Assuming you want to initialize an empty dictionary before returning:
+            MetaData.ScoreData = new Dictionary<string, float>();
+
+            return MetaData;
         }
     }
 }
