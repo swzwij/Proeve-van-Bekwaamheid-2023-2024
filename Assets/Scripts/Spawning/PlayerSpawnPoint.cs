73a2e8edf86e1e34577a9bb081469b103676c15f
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2024-03-13 13:05:16 +0100

    Reverted player size on prefab in scene

--- Assets/Scripts/Spawning/PlayerSpawnPoint.cs
+++ Assets/Scripts/Spawning/PlayerSpawnPoint.cs
@@ -5,18 +5,22 @@ namespace UntitledCube.Spawning
 {
     public class PlayerSpawnPoint : MonoBehaviour
     {
-        [SerializeField] private PlayerWrapper _player;
+        [SerializeField] private GameObject _player;
         [SerializeField] private Transform _spawnPoint;
+
+        private PlayerWrapper _playerWrapper;
 
         private void OnEnable() => MazeGenerator.OnGenerated += SpawnPlayer;
 
         private void OnDisable() => MazeGenerator.OnGenerated -= SpawnPlayer;
 
+        private void Awake() => _playerWrapper = _player.GetComponent<PlayerWrapper>();
+
         private void SpawnPlayer(string _)
         {
             _player.transform.SetPositionAndRotation(_spawnPoint.position, Quaternion.identity);
             
-            foreach (Rigidbody rigidbody in _player.Rigidbodies)
+            foreach (Rigidbody rigidbody in _playerWrapper.Rigidbodies)
             {
                 rigidbody.velocity = Vector3.zero;
                 rigidbody.freezeRotation = true;
